3ba257d404eba60b7c273d9f87ca7bf674e6ba26
Rafazg <claudenirorafaelbjj@gmail.com>
2026-05-18 03:15:55 -0300

    fix: adicionada URI no client

--- src/main/java/com/zgdev/agendador_tarefas/infrastructure/security/SecurityConfig.java
+++ src/main/java/com/zgdev/agendador_tarefas/infrastructure/security/SecurityConfig.java
@@ -3,16 +3,11 @@ package com.zgdev.agendador_tarefas.infrastructure.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -21,12 +16,12 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class SecurityConfig {
 
     // Instâncias de JwtUtil e UserDetailsService injetadas pelo Spring
-    private final com.zgdev.usuario.Infrastructure.security.JwtUtil jwtUtil;
-    private final UserDetailsService userDetailsService;
+    private final com.zgdev.agendador_tarefas.infrastructure.security.JwtUtil jwtUtil;
+    private final UserDetailsServiceImpl userDetailsService;
 
     // Construtor para injeção de dependências de JwtUtil e UserDetailsService
     @Autowired
-    public SecurityConfig(com.zgdev.usuario.Infrastructure.security.JwtUtil jwtUtil, UserDetailsService userDetailsService) {
+    public SecurityConfig(com.zgdev.agendador_tarefas.infrastructure.security.JwtUtil jwtUtil, UserDetailsServiceImpl userDetailsService) {
         this.jwtUtil = jwtUtil;
         this.userDetailsService = userDetailsService;
     }
